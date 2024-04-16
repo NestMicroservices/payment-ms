f2354b8efc8af83a5e5d43e2930f5187cc308f33
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-04-16 14:16:10 -0500

    Connect stripeWebhook

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,19 +4,25 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   STRIPE_SECRET: string;
+  STRIPE_SUCCESS_URL: string;
+  STRIPE_CANCEL_URL: string;
+  STRIPE_ENDPOINT_SECRET: string;
 }
 
 const envsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().required(),
     STRIPE_SECRET: joi.string().required(),
+    STRIPE_SUCCESS_URL: joi.string().required(),
+    STRIPE_CANCEL_URL: joi.string().required(),
+    STRIPE_ENDPOINT_SECRET: joi.string().required(),
   })
   .unknown(true);
 
 const { error, value } = envsSchema.validate(process.env);
 
 if (error) {
-  throw new Error(`Config validation error: \\\${error.message}`);
+  throw new Error(`Config validation error: ${error.message}`);
 }
 
 const envVars: EnvVars = value;
@@ -24,4 +30,7 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   stripeSecret: envVars.STRIPE_SECRET,
+  stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
+  stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
+  stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
 };
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 import Stripe from 'stripe';
+import { Request, Response } from 'express';
+
 import { envs } from 'src/config';
 import { PaymentSessionDto } from './dto/payment-session.dto';
 
@@ -9,7 +11,7 @@ export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
 
   async createPaymentSession(paymentSessioDto: PaymentSessionDto) {
-    const { currency, items } = paymentSessioDto;
+    const { currency, items, orderId } = paymentSessioDto;
 
     const lineItems = items.map((item) => ({
       price_data: {
@@ -24,14 +26,48 @@ export class PaymentsService {
 
     const session = await this.stripe.checkout.sessions.create({
       payment_intent_data: {
-        metadata: {},
+        metadata: {
+          orderId: orderId,
+        },
       },
       line_items: lineItems,
       mode: 'payment',
-      success_url: 'http://localhost:3003/payments/success',
-      cancel_url: 'http://localhost:3003/payments/cancel',
+      success_url: envs.stripeSuccessUrl,
+      cancel_url: envs.stripeCancelUrl,
     });
 
     return session;
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const sig = req.headers['stripe-signature'];
+    const endpointSecret = envs.stripeEndpointSecret;
+    let event: Stripe.Event;
+
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        endpointSecret,
+      );
+    } catch (err) {
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    switch (event.type) {
+      case 'charge.succeeded':
+        const chargeSucceeded = event.data.object;
+        //  TODO: call to microservice
+        console.log({
+          metadata: chargeSucceeded.metadata,
+          orderId: chargeSucceeded.metadata.orderId,
+        });
+        break;
+
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    return res.status(200).json({ sig });
   }
 }
